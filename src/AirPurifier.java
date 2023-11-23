44320bab9d6015b95d858bcccdacd25072292273
Punnawich Mai <punnawich_dachin@gmail.com>
2023-11-23 13:19:59 +0700

    Finished

---
+++ src/AirPurifier.java
@@ -0,0 +1,67 @@
+import java.util.Objects;
+
+public class AirPurifier {
+    String model;
+    String SN;
+    boolean power;
+    int temp,den,FanSP;
+    String Mode = "";
+
+    void turnOn(){
+        power = true;
+        temp = 27;
+        den = 10;
+        FanSP = 1;
+        Mode = "Normal";
+    }
+    void turnOff(){
+        power = false;
+        temp = 0;
+        den = 0;
+        FanSP = 0;
+        Mode = "";
+    }
+    void changeFanSpeed(){
+        FanSP++;
+        if(FanSP>3){
+            FanSP=1;
+        }
+    }
+    void changeMode(){
+        if(Objects.equals(Mode, "Normal")){
+            Mode = "Performance";
+            FanSP = 3;
+        }
+        else if(Objects.equals(Mode, "Performance")){
+            Mode = "Silence";
+            FanSP = 1;
+        }
+        else if(Objects.equals(Mode, "Silence")){
+            Mode = "Normal";
+        }
+    }
+    void ShowScreen(){
+        if(power) {
+            System.out.println("Power: On");
+        }
+        else{
+            System.out.println("Power: Off");
+        }
+        System.out.println("Temperature: "+temp+" °C");
+        System.out.println("PM2.5 Density: "+den+" μg/m³");
+        System.out.println("Fan Speed: "+FanSP);
+        System.out.println("Mode: "+Mode);
+        System.out.println("---------------");
+    }
+
+    AirPurifier(String SN){
+        model = "Pro"; this.SN = SN;
+    }
+    AirPurifier(String model, String SN) {
+        this.model = model; this.SN = SN;
+
+    }
+
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,60 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        test01();
+        test02();
+        test03();
     }
+    public static void test01(){ // Change fan speed testing
+        AirPurifier a = new AirPurifier("Pro","SN00000001");
+        a.ShowScreen();
+        a.turnOn();
+        a.ShowScreen();
+
+        a.changeFanSpeed();
+        a.ShowScreen();
+        a.changeFanSpeed();
+        a.ShowScreen();
+        a.changeFanSpeed();
+        a.ShowScreen();
+
+        a.turnOff();
+        a.ShowScreen();
+    }
+
+    public static void test02(){ // Change mode testing
+        AirPurifier a = new AirPurifier("Pro","SN00000001");
+        a.ShowScreen();
+        a.turnOn();
+        a.ShowScreen();
+
+        a.changeMode();
+        a.ShowScreen();
+        a.changeMode();
+        a.ShowScreen();
+        a.changeMode();
+        a.ShowScreen();
+
+        a.turnOff();
+        a.ShowScreen();
+    }
+    public static void test03(){ // Change mode testing
+        AirPurifier a = new AirPurifier("SN00000001");
+        a.ShowScreen();
+        a.turnOn();
+        a.ShowScreen();
+
+        a.changeMode();
+        a.ShowScreen();
+        a.changeFanSpeed();
+        a.ShowScreen();
+        a.changeMode();
+        a.ShowScreen();
+        a.changeFanSpeed();
+        a.ShowScreen();
+
+        a.turnOff();
+        a.ShowScreen();
+    }
+
 }
